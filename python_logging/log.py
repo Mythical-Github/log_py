b50768ed9fe4902c3879fe3c24dedff24086e26d
Mythical-Github <MythicalData@gmail.com>
2024-08-05 17:27:48 -0400

    fixed bug

--- python_logging/log.py
+++ python_logging/log.py
@@ -55,7 +55,7 @@ def rename_latest_log(log_dir):
     if os.path.isfile(latest_log_path):
         try:
             timestamp = datetime.now().strftime('%m_%d_%Y_%H%M_%S')
-            new_name = f'log_prefix{timestamp}.log'
+            new_name = f'{log_prefix}{timestamp}.log'
             new_log_path = os.path.join(log_dir, new_name)
             os.rename(latest_log_path, new_log_path)
         except PermissionError as e:
