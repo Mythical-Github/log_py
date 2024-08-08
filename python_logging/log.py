abbacca32a439516ca8b8c5db778243c45d7bae8
Mythical-Github <MythicalData@gmail.com>
2024-08-08 18:29:51 -0400

    Update log.py

--- python_logging/log.py
+++ python_logging/log.py
@@ -10,29 +10,21 @@ init(autoreset=True)
 
 logger = logging.getLogger(__name__)
 
-
 log_base_dir = f'{os.getcwd()}/src'
-
 colors_json_path = f'{log_base_dir}/log_colors.json'
-
 colors_config = ''
 theme_colors = ''
 default_color = ''
 background_color = ''
 log_prefix = ''
-
-
 has_configured_logging = False
-
-
-def module_setup():
-    return
+inter_log = ''
+max_terminal_width = get_terminal_size().columns
 
 
 def set_log_base_dir(base_dir: str):
     global log_base_dir
     log_base_dir = base_dir
-
 
 def set_colors_json_path(json_path: str):
     global colors_json_path
@@ -43,21 +35,19 @@ def set_colors_json_path(json_path: str):
     else:
         colors_json_path = json_path
 
-
 def load_theme_colors():
     if not os.path.isfile(colors_json_path):
         raise FileNotFoundError(f"Theme colors file not found: {colors_json_path}")
     with open(colors_json_path, 'r') as f:
         return json.load(f)
 
-
 def configure_logging():
-
     global colors_config
     global theme_colors
     global default_color
     global background_color
     global log_prefix
+    global inter_log
 
     colors_config = load_theme_colors()
     theme_colors = colors_config.get('theme_colors', {})
@@ -76,48 +66,53 @@ def configure_logging():
     rename_latest_log(log_dir)
 
     original_path = os.path.join(log_dir, 'latest.log')
-
-    global inter_log
     inter_log = original_path
 
-    log_file = inter_log
-
-    file_handler = logging.FileHandler(log_file)
+    file_handler = logging.FileHandler(inter_log)
     file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter('%(message)s'))
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
 
     logger.addHandler(file_handler)
     logger.setLevel(logging.INFO)
 
-
 def log_message(message: str):
     global has_configured_logging
+    global max_terminal_width
+    
     if not has_configured_logging:
         configure_logging()
         has_configured_logging = True
+
+    current_width = get_terminal_size().columns
+    max_terminal_width = max(max_terminal_width, current_width)
+
     logger.info(message)
+
     color = default_color
     for keyword, assigned_color in theme_colors.items():
         if keyword in message:
             color = assigned_color
             break
-    terminal_width = get_terminal_size().columns
-    padded_message = (message[:terminal_width] if len(message) > terminal_width else message.ljust(terminal_width))
+
+    padded_message = (message[:max_terminal_width] if len(message) > max_terminal_width else message.ljust(max_terminal_width))
     print(f"{background_color}{color}{padded_message}{Style.RESET_ALL}")
-
-
 
 def rename_latest_log(log_dir):
     latest_log_path = os.path.join(log_dir, 'latest.log')
     if os.path.isfile(latest_log_path):
         try:
-            timestamp = datetime.now().strftime('%m_%d_%Y_%H%M_%S')
-            new_name = f'{log_prefix}{timestamp}.log'
-            new_log_path = os.path.join(log_dir, new_name)
-            os.rename(latest_log_path, new_log_path)
+            with open(latest_log_path, 'a') as f:
+                temp_name = f'{log_prefix}temp_{datetime.now().strftime("%m_%d_%Y_%H%M_%S")}.log'
+                temp_log_path = os.path.join(log_dir, temp_name)
+
+                os.rename(latest_log_path, temp_log_path)
+
+                timestamp = datetime.now().strftime('%m_%d_%Y_%H%M_%S')
+                final_name = f'{log_prefix}{timestamp}.log'
+                final_log_path = os.path.join(log_dir, final_name)
+                os.rename(temp_log_path, final_log_path)
+
         except PermissionError as e:
             log_message(f"Error renaming log file: {e}")
-            import time
-            time.sleep(1)
-            rename_latest_log(log_dir)
-            return
+        except Exception as e:
+            log_message(f"Unexpected error renaming log file: {e}")
